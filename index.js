95eb8908b129c06a7e2a7235762f6353dcce3c88
William <wyvargasc@unjbg.edu.pe>
2024-01-20 11:09:24 -0500

    Cat over cubes
    Cat

---
+++ index.js
@@ -0,0 +1,19 @@
+function cube(){
+    let e = document.createElement('div');
+    e.setAttribute('class','cube');
+    document.body.appendChild(e);
+    e.style.left = Math.random() * + innerWidth + 'px';
+    
+    let size = Math.random() * 12;
+    let duration = Math.random() * 3;
+    e.style.fontSize = 12  + size+'px';
+    e.style.animationDuration = 2 + duration+'s';
+
+    setTimeout(function() {
+        document.body.removeChild(e);
+    }, 5000);
+}
+
+setInterval(function(){
+    cube()
+}, 100);
